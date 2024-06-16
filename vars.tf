f0cf405ebb302e694d3d395530d504734b875a91
Thomas Hamer <thomas_hamer@outlook.com>
2024-06-16 18:17:51 +1000

    remove vars

--- vars.tf
+++ vars.tf
@@ -3,16 +3,6 @@ variable "domain_name" {
   type        = string
 }
 
-variable "validation_method" {
-  description = "Method for validating the domain name (DNS or EMAIL)"
-  type        = string
-}
-
-variable "route53_zone_id" {
-  description = "The ID of the Route 53 hosted zone to create the validation record in"
-  type        = string
-}
-
 variable "tags" {
   description = "A map of tags to assign to the resources"
   type        = map(string)
@@ -22,11 +12,7 @@ variable "tags" {
 variable "lambda_zip_path" {
   description = "The path to the Lambda zip file"
   type        = string
-}
-
-variable "cloudfront_distribution_hosted_zone_id" {
-  description = "The hosted zone ID of the CloudFront distribution"
-  type        = string
+  default     = "./secheaders.py"
 }
 
 variable "create_logging_bucket" {
