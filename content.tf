1bd286860728bf633b88a3e0b17f161778145bf7
Thomas Hamer <thomas_hamer@outlook.com>
2024-06-22 13:28:52 +1000

    add content

---
+++ content.tf
@@ -0,0 +1,18 @@
+
+resource "null_resource" "clone_html_repo" {
+  provisioner "local-exec" {
+    command = <<EOT
+      rm -rf ${path.module}/cloned_html_repo
+      git clone --branch ${var.html_source_git_branch} ${var.html_source_git_repo_url} ${path.module}/cloned_html_repo
+    EOT
+  }
+}
+
+resource "null_resource" "upload_html_files" {
+  depends_on = [module.s3, null_resource.clone_html_repo]
+  provisioner "local-exec" {
+    command = <<EOT
+      aws s3 sync ${path.module}/cloned_html_repo s3://${aws_s3_bucket.website_bucket.bucket}
+    EOT
+  }
+}
