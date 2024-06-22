a7b740a466b9d544fe3b5ada716fb550a7b98d57
Thomas Hamer <thomas_hamer@outlook.com>
2024-06-22 14:05:37 +1000

    remove-vars

--- vars.tf
+++ vars.tf
@@ -9,17 +9,6 @@ variable "create_logging_bucket" {
   default     = false
 }
 
-variable "html_source_git_repo_url" {
-  description = "The URL of the Git repository containing the HTML source code"
-  type        = string
-}
-
-variable "html_source_git_branch" {
-  description = "The branch of the Git repository containing the HTML source code"
-  type        = string
-  default     = "main"
-}
-
 variable "tags" {
   description = "A map of tags to assign to the resources"
   type        = map(string)
